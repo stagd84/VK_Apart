e629679cda4663b17fe0e0e4ead43b4580876049
stagd84 <d.stangonis@gmail.com>
2025-05-03 23:07:56 +0300

    Update and rename MyApart_App.py to 1_📂file.py
    trying fixing error

--- pages/1_📂file.py
+++ pages/1_📂file.py
@@ -8,8 +8,8 @@ Created on Sat Aug 31 12:37:10 2024
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title='Home', page_icon='🏠')
-st.title("🏠  Home")
+st.set_page_config(page_title='Select File', page_icon='📂')
+st.title("📂  Select File")
 
 # Initialize session state for the uploaded file and DataFrame
 if 'uploaded_file' not in st.session_state:
